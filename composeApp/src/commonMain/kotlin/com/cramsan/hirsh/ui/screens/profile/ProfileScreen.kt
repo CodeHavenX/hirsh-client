dde02c50ba5a94f8562d3b3653b300f2bc236e6e
Cesar Ramirez <contact@cramsan.com>
2026-09-18 16:19:30 -0700

    Remove ProfileScreen.kt's now-unused Role.toDisplayLabel import
    Left behind when the role display switched to the raw roles array.

--- composeApp/src/commonMain/kotlin/com/cramsan/hirsh/ui/screens/profile/ProfileScreen.kt
+++ composeApp/src/commonMain/kotlin/com/cramsan/hirsh/ui/screens/profile/ProfileScreen.kt
@@ -37,7 +37,6 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.cramsan.hirsh.model.toDisplayLabel
 import com.cramsan.hirsh.ui.components.BadgeTone
 import com.cramsan.hirsh.ui.components.KeyValueRow
 import com.cramsan.hirsh.ui.components.StatusBadge
